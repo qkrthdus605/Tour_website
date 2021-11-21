a23f64982acb309c10e518aaa2ab57c28e64a12d
qkrthdus605 <qkrthdus605@gmail.com>
2021-11-22 00:52:01 +0900

    추가 : MainPage 생성

---
+++ src/pages/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from '../components/Header';
+
+const MainPage = () => {
+  return(
+    <>
+     <Header />
+     <h1>Main</h1>
+    </>
+  );
+};
+
+export default MainPage;
